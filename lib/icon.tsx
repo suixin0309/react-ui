5d1df595ff8565d6587e650463bac68399bf8a8b
suixin <suixin0309@163.com>
2019-05-10 18:04:08 +0800

    添加 icon.scss

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import './importicons'
-
+import './icon.scss'
 interface IconProps {
     name: string
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <span>
-            <svg>
-                <use xlinkHref={`#${props.name}`} />
-            </svg>
-        </span>
+        <svg className='sui-icon'>
+            <use xlinkHref={`#${props.name}`} />
+        </svg>
 
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
+
 module.exports = {
     // mode: "production",
     entry: {
@@ -10,7 +12,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
-        library: 'Sui',
+        library: 'sui',
         libraryTarget: "umd"
     },
     module: {
@@ -22,6 +24,23 @@ module.exports = {
             {
                 test: /icons.+\.svg$/,
                 loader: 'svg-sprite-loader',
+            },
+            {
+                test: /\.s([ac])ss$/,
+                use: [
+                    devMode ? 'style-loader' : {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            includePaths: [path.resolve(__dirname, 'stylesheets', 'include')]
+                        }
+                    }]
             }
         ]
     },
