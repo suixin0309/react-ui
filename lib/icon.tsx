02dc9eceb02ba62164cc6100e90bd67cfc49270a
suixin <suixin0309@163.com>
2019-05-13 11:27:12 +0800

    icon 简化参数  函数参数结构赋值

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import './importicons'
-import './icon.scss'
-import './helpers/classnames'
+import './importicons';
+import './icon.scss';
+import './helpers/classnames';
 import classes from './helpers/classnames';
-interface IconProps extends React.SVGAttributes<SVGElement>{
+
+interface IconProps extends React.SVGAttributes<SVGElement> {
     name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-    const {className,...restProps}=props
+const Icon: React.FunctionComponent<IconProps> = ({className, name, ...restProps}) => {
     return (
-        <svg className={classes('sui-icon',className)} {...restProps}>
-            <use xlinkHref={`#${props.name}`} />
+        <svg className={classes('sui-icon', className)} {...restProps}>
+            <use xlinkHref={`#${name}`}/>
         </svg>
 
     );
