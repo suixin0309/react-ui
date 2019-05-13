94a632e8fa93787fc68b69e30735c892bd96da69
suixin <suixin0309@163.com>
2019-05-13 18:17:07 +0800

    ci 发布

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,3 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-
-// import Button from './button';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e);
-    console.log((e.target as SVGUseElement).href);   //断言
-};
-ReactDom.render(
-    <div>
-        <Icon name='qq' onClick={fn} onMouseEnter={()=>console.log("enter")}/>
-        <Icon name='wechat' onClick={fn} onMouseEnter={()=>console.log("enter")}/>
-    </div>, document.getElementById('root'));
+// import Icon from './icon/icon';
+// export {Icon}
+export {default as Icon} from './icon/icon'
