0f98a7549bfd0b4e5d73baa90c8c29c458e30b34
suixin <suixin0309@163.com>
2019-05-28 11:47:36 +0800

    remove all files

--- jest.config.ci.js
+++
@@ -1,8 +0,0 @@
-const base = require('./jest.config')
-module.exports = Object.assign({}, base, {
-    reporters: ["default", "jest-junit"],
-    collectCoverage: true,
-    collectCoverageFrom: ["{lib,include}/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-})
--- tests/hello1.unit.tsx
+++
@@ -1,5 +0,0 @@
-describe("hello",()=>{
-    it("1==2",()=>{
-        expect(1).toEqual(2)
-    })
-})
